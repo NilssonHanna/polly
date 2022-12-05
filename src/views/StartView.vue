919ee51feda110dd9703620e5e7c487ee60c1618
linnspace01 <linn.gattermann.1507@student.uu.se>
2022-12-05 14:31:49 +0100

    ändringar start 2

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,51 +1,33 @@
 <template>
   <body>
-    <div v-bind:class="['hamburger', {'close': !hideNav}]"
-         v-on:click="toggleNav">
+    <div class="logo"> 
+      Xplanandum 
     </div>
-    <div class="logo"> WELCOME TO XPLANANDUM</div>
+
+  <button id="language" v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+
+  <div id="create">
+    <router-link class="link" v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link> 
+  </div>
+
   
-   <!--<div>
-      <label>
-        Write poll id:
-        <input type="text" v-model="id">
-      </label>
-        <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link>
-    </div>-->
-  <ResponsiveNav v-bind:hideNav="hideNav">
-    <button id="language" v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
-    <router-link id="create" v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
-  </ResponsiveNav>
-  
-  <label>
-    Write poll id:
-    <input type="text" v-model="id">
-  </label>
-  <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link>
- <label>
-     Write poll id:
-    <input type="text" v-model="id">
-  </label>
-  <router-link v-bind:to="'/nickname/'+id">{{uiLabels.participatePoll}}</router-link>
- </body>
- </template>
-  
- <script>
- import ResponsiveNav from '@/components/ResponsiveNav.vue';
- import io from 'socket.io-client';
- const socket = io();
-  
- export default {
+  <div id="join">
+    <router-link class="link" v-bind:to="'/join/'+lang" >{{uiLabels.participatePoll}}</router-link> 
+  </div>
+
+</body>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const socket = io();
+
+export default {
   name: 'StartView',
-  components: {
-    ResponsiveNav
-  },
   data: function () {
     return {
       uiLabels: {},
-      id: "",
       lang: "en",
-      hideNav: true
     }
   },
   created: function () {
@@ -61,13 +43,10 @@
         this.lang = "en"
       socket.emit("switchLanguage", this.lang)
     },
-    toggleNav: function () {
-      this.hideNav = ! this.hideNav;
-    }
   }
- }
- </script>
- <style scoped>
+}
+</script>
+<style scoped>
   body {
     background-color: rgb(161, 255, 160);
     width: 100%;
@@ -75,35 +54,20 @@
     grid-template-columns: 2em auto;
     min-height: 100vh;
   }
-   .logo {
+  
+  .logo {
     text-transform: uppercase;
-    margin: 300px;
-    letter-spacing: 0.1em;
-    font-size: 3rem;
+    margin-left: 350px;
+    margin-top: 120px;
+    letter-spacing: 0.2em;
+    font-size: 4rem;
     color: rgb(0, 0, 0);
     padding-top:0.2em;
     font-family: "Fjord one";
     font-weight: bold;
- }
-   /*.logo img {
-    height:2.5rem;
-    vertical-align: bottom;
-    margin-right: 0.5rem;
-  } */
-  .hamburger {
-    color:rgb(138, 246, 155);
-    width:1em;
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    padding:0.5rem;
-    top:0;
-    left:0;
-    height: 2rem;
-    cursor: pointer;
-    font-size: 1.5rem;
-  }
+}
   
+
   #language {
   background-color: rgb(254, 190, 201);
   font-size: 1.5rem;
@@ -111,36 +75,62 @@
   padding: 25px;
   position: absolute;
   top: -10px;
-  right: -10px;
+  right: 0px;
   font-family: "Fjord one";
- }
-  
- #create {
+  cursor:pointer;
+}
+
+#create {
   background-color: rgb(90, 58, 64);
   font-size: 1.5rem;
   color: rgb(255, 255, 255);
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
   padding: 30px;
-  margin-top: -200px;
+  margin-top: 300px;
   position: absolute;
   left: 40%;
+  width:100px;
   transform: translateX(-50%);
   font-family: "Fjord one";
- }
-  
- #join {
+
+}
+
+.link{
+  color: rgb(255, 255, 255);
+  text-decoration: none;
+}
+
+/*#or{
+  margin-left: 530px;
+  margin-top: 10px;
+  font-size: 1.5rem;
+  font-family: "Fjord one"; 
+}
+
+/*#pollid {
+  font-family: "Fjord one";
+  font-size: 1.5rem;
+}*/
+
+#join {
   background-color: rgb(90, 58, 64);
   font-size: 1.5rem;
   color: rgb(255, 255, 255);
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
   padding: 30px;
-  margin-top: -200px;
+  margin-top: 300px;
   position: absolute;
   left: 60%;
+  width:100px;
   transform: translateX(-50%);
   font-family: "Fjord one";
- }
-  
-  
- @media screen and (max-width:50em) {
+}
+
+
+
+@media screen and (max-width:50em) {
   .logo {
     font-size: 5vw;
     display: flex;
@@ -156,7 +146,6 @@
   .hide {
     left:-12em;
   }
-  
- }
- </style>
- 
+
+}
+</style>
