3bce49fc9d9b459df99c28de141ec1962423eb3d
Hanna Nilsson <hannanilsson@Hannas-MacBook-Pro.local>
2022-12-06 15:17:31 +0100

    nickname med rätt startview

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,51 +1,30 @@
 <template>
   <body>
-    <div v-bind:class="['hamburger', {'close': !hideNav}]"
-         v-on:click="toggleNav">
+    <div class="logo"> 
+      Xplanandum 
     </div>
-    <div class="logo"> WELCOME TO XPLANANDUM</div>
-  
-   <!--<div>
-      <label>
-        Write poll id:
-        <input type="text" v-model="id">
-      </label>
-        <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link>
-    </div>-->
-  <ResponsiveNav v-bind:hideNav="hideNav">
-    <button id="language" v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
-    <router-link id="create" v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link>
-  </ResponsiveNav>
-  
-  <label>
-    Write poll id:
-    <input type="text" v-model="id">
-  </label>
-  <router-link v-bind:to="'/poll/'+id">{{uiLabels.participatePoll}}</router-link>
- <label>
-     Write poll id:
-    <input type="text" v-model="id">
-  </label>
-  <router-link v-bind:to="('/nickname/'+lang)">{{uiLabels.participatePoll}}</router-link>
- </body>
- </template>
-  
- <script>
- import ResponsiveNav from '@/components/ResponsiveNav.vue';
- import io from 'socket.io-client';
- const socket = io();
-  
- export default {
+
+  <button id="language" v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
+
+
+  <div >
+    <router-link id="create" v-bind:to="'/create/'+lang">{{uiLabels.createPoll}}</router-link> 
+
+    <router-link id="join" v-bind:to="'/join/'+lang" >{{uiLabels.participatePoll}}</router-link> 
+  </div>
+
+</body>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const socket = io();
+export default {
   name: 'StartView',
-  components: {
-    ResponsiveNav
-  },
   data: function () {
     return {
       uiLabels: {},
-      id: "",
       lang: "en",
-      hideNav: true
     }
   },
   created: function () {
@@ -60,10 +39,8 @@
       else
         this.lang = "en"
       socket.emit("switchLanguage", this.lang)
+      console.log(this.lang)
     },
-    toggleNav: function () {
-      this.hideNav = ! this.hideNav;
-    }
   }
  }
  </script>
@@ -77,32 +54,16 @@
   }
    .logo {
     text-transform: uppercase;
-    margin: 300px;
-    letter-spacing: 0.1em;
-    font-size: 3rem;
+    margin-left: 350px;
+    margin-top: 120px;
+    letter-spacing: 0.2em;
+    font-size: 4rem;
     color: rgb(0, 0, 0);
-    padding-top:0.2em;
     font-family: "Fjord one";
     font-weight: bold;
- }
-   /*.logo img {
-    height:2.5rem;
-    vertical-align: bottom;
-    margin-right: 0.5rem;
-  } */
-  .hamburger {
-    color:rgb(138, 246, 155);
-    width:1em;
-    display: flex;
-    align-items: center;
-    justify-content: left;
-    padding:0.5rem;
-    top:0;
-    left:0;
-    height: 2rem;
-    cursor: pointer;
-    font-size: 1.5rem;
-  }
+    position: absolute;
+    text-align: center;
+}
   
   #language {
   background-color: rgb(254, 190, 201);
@@ -111,36 +72,42 @@
   padding: 25px;
   position: absolute;
   top: -10px;
-  right: -10px;
+  right: 0px;
   font-family: "Fjord one";
- }
-  
- #create {
+  cursor:pointer;
+ 
+}
+#create {
   background-color: rgb(90, 58, 64);
   font-size: 1.5rem;
   color: rgb(255, 255, 255);
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
   padding: 30px;
-  margin-top: -200px;
+  margin-top: 300px;
   position: absolute;
   left: 40%;
+  width:100px;
   transform: translateX(-50%);
   font-family: "Fjord one";
- }
-  
- #join {
+  text-decoration: none;
+}
+#join {
   background-color: rgb(90, 58, 64);
   font-size: 1.5rem;
   color: rgb(255, 255, 255);
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
   padding: 30px;
-  margin-top: -200px;
+  margin-top:300px;
   position: absolute;
   left: 60%;
+  width:100px;
   transform: translateX(-50%);
   font-family: "Fjord one";
- }
-  
-  
- @media screen and (max-width:50em) {
+  text-decoration: none;
+}
+@media screen and (max-width:50em) {
   .logo {
     font-size: 5vw;
     display: flex;
