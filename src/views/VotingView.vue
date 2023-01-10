ef87a45491190c202897181305c5987a49d7adb8
Hanna Nilsson <hannanilsson@uu-guest-155-189.eduroam.uu.se>
2023-01-10 13:12:30 +0100

    blaba

--- src/views/VotingView.vue
+++ src/views/VotingView.vue
@@ -7,6 +7,7 @@
     <div>
     <div id="playervote">
       <h1> {{uiLabels.voting}}</h1>
+      <h5> {{this.nicknameVotes}}</h5>
       
     </div>
 
@@ -16,6 +17,10 @@
       <form>
         <div v-for="(explanation, index) in allexplanations" :key="index+explanation"> 
         <input type="radio" v-bind:value="explanation" v-model="selectedOption"/>
+        <div v-if="selectedOption = nicknameVotes.nExplanations.playerExplanation"
+        nicknameVotes.nVote ++
+        
+        ></div>
         {{ explanation }} 
         </div>
       </form>
